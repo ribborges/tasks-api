c633b51a99e6ab8d5d3dec3621357a945f21eeeb
Richard <ribborges@outlook.com>
2024-05-15 00:12:21 -0300

    chore!: setup express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log('Hello, World!');
+import express from "express";
+import cors from "cors";
+
+// Set up the express app
+const app = express();
+const port = 3000;
+const allowedOrigins = ["http://localhost:5173"];
+const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+};
+
+app.use(cors(options));
+app.use(express.json());
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
