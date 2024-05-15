00693f9acdd74fad4481ef62afe9939520e38a67
Richard <ribborges@outlook.com>
2024-05-15 06:27:39 -0300

    fix: mongodb url

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,9 +1,9 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dlbqmzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://ribborges:9d7aSKQMKgglHhjG@cluster0.dlbqmzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(uri!, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
