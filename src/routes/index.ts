a55ee345344b41ddf0fb446d2535ee6fd1780570
Richard Borges <ribborges@outlook.com>
2025-01-17 16:31:27 -0300

    refactor: change tasks module for improved structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import auth from "./auth";
+import tasks from "./tasks";
 
 const router = Router();
 
 export default (): Router => {
     auth(router);
+    tasks(router);
 
     return router;
 }
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,13 +1,11 @@
-import express from "express";
+import { Router } from "express";
 
 import { completeTask, createTask, deleteTask, getTasks, updateTask } from "@/controllers/tasks";
 
-const router = express.Router();
-
-router.post("/tasks", createTask);
-router.get("/tasks", getTasks);
-router.put("/tasks/:id", updateTask);
-router.put("/complete-task/:id", completeTask);
-router.delete("/tasks/:id", deleteTask);
-
-export default router;
+export default (router: Router) => {
+    router.post("/tasks", createTask);
+    router.get("/tasks", getTasks);
+    router.put("/tasks/:id", updateTask);
+    router.put("/complete-task/:id", completeTask);
+    router.delete("/tasks/:id", deleteTask);
+};
