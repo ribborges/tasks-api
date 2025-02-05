960e8179883f249e0c097f343e6fb188fca1ab94
Richard Borges <ribborges@outlook.com>
2025-02-04 23:52:52 -0300

    feat: add client URL support for CORS configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,5 +5,6 @@ dotenv.config();
 const dbURI = process.env.MONGO_URI;
 const appPort = process.env.PORT;
 const secret = process.env.SECRET ?? '';
+const clientURL = process.env.CLIENT_URL?.split(',') ?? [];
 
-export { dbURI, appPort, secret };
+export { dbURI, appPort, secret, clientURL };
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,19 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 
 import router from '@/routes';
-import { appPort } from "@/config/env";
+import { appPort, clientURL } from "@/config/env";
 import { connectDB } from "@/database/operations";
 
 // Set up the express app
 const app = express();
 const port = appPort || 8000;
 
-app.use(cors());
+app.use(cors({
+    origin: clientURL,
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
